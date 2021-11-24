61b2def02987c9edd6aeef25db419aed3b2b413d
Ulugbek <61221916+ulugbek15@users.noreply.github.com>
2021-11-24 18:28:38 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import {Routes, Route, Link} from 'react-router-dom';
 import './App.css';
+import Profile from './Pages/Ptofile/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Link to="/profile">Profile</Link>
+    <Routes>
+      <Route path="/profile" element={<Profile/>}/>
+    </Routes>
+    </>
   );
 }
 
